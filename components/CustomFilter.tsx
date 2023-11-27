4c338bd7e792293c2a9479a67b51c55cd3d9564f
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-11-27 16:57:01 +0200

    added functionality to custom filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,12 +4,26 @@ import { useRouter } from "next/navigation";
 import { CustomFilterProps } from "@/types";
 import { Listbox, Transition } from "@headlessui/react";
 import Image from "next/image";
+import { updateSearchParams } from "@/utils";
 
 function CustomFilter({ title, options }: CustomFilterProps) {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName);
+
+    router.push(newPathName, { scroll: false });
+  };
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
